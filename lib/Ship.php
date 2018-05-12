a050158c08630122886443065640656a87c9a51a
VladXII <PobereznichenkoVladimir@gmail.com>
2018-05-12 16:55:50 +0300

    initial commit

---
+++ lib/Ship.php
@@ -0,0 +1,48 @@
+<?php
+
+class Ship
+{
+    public $name;
+
+    public $weapon_power = 0;
+
+    public $jedi_factor = 0;
+
+    public $strength = 0;
+
+    public function sayHello()
+    {
+        echo 'Hello';
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getNameAndSpecs($useShortForm)
+    {
+        if ($useShortForm) {
+            return sprintf(
+                '%s: w:%s, j:%s, s:%s',
+                $this->name,
+                $this->weapon_power,
+                $this->jedi_factor,
+                $this->strength
+            );
+        } else {
+            return sprintf(
+                '%s: w:%s/j:%s/s:%s',
+                $this->name,
+                $this->weapon_power,
+                $this->jedi_factor,
+                $this->strength
+            );
+        }
+    }
+
+    public function doesGivenShipHasMoreStrenght($givenShip)
+    {
+        return $givenShip->strength > $this->strength;
+    }
+}
--- play.php
+++ play.php
@@ -1,3 +1,41 @@
 <?php
 
-echo 'It\'s a trap!!!';
+require_once __DIR__.'/lib/Ship.php';
+
+/**
+ * @param Ship $someShip
+ */
+
+function printShipSummary($someShip)
+{
+    echo 'star ship: '.$someShip->name;
+    echo '<hr/>';
+    $someShip->sayHello();
+    echo '<hr/>';
+    echo $someShip->getName();
+    echo '<hr/>';
+    echo $someShip->getNameAndSpecs(false);
+    echo '<hr/>';
+    echo $someShip->getNameAndSpecs(true);
+}
+
+$myShip = new Ship();
+$myShip->name = 'Jedi Starship';
+$myShip->weapon_power = 10;
+
+$otherShip = new Ship();
+$otherShip->name = 'Imperial Shuttle';
+$otherShip->weapon_power = 5;
+$otherShip->strength = 50;
+
+printShipSummary($myShip);
+echo '<hr/>';
+printShipSummary($otherShip);
+echo '<hr/>';
+
+if ($myShip->doesGivenShipHasMoreStrenght($otherShip)) {
+    echo $otherShip->name.' is more powerful';
+} else {
+    echo $myShip->name.' is more powerful';
+}
+
