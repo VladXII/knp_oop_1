bc56abd4963a42344ccf64263094ba078b6d55e6
VladXII <PobereznichenkoVladimir@gmail.com>
2018-05-16 12:12:55 +0300

    initial commit

--- battle.php
+++ battle.php
@@ -1,6 +1,6 @@
 <?php
 require __DIR__.'/bootstrap.php';
-require_once __DIR__.'/lib/BattleManager.php';
+require_once __DIR__.'/lib/Service/BattleManager.php';
 
 $container = new Container($configuration);
 $shipLoader = $container->getShipLoader();
